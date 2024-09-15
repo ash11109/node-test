0c39d58f07cbf1e67437d062ad8c3de2c7139375
ash11109 <ayushcool1275@gmail.com>
2024-09-15 17:47:40 +0530

    9

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
+const server = http.createServer(app);  // Use the server instance for both HTTP and WebSockets
 const { Server } = require("socket.io");
+const cors = require('cors');
 
 let devices = []; 
 let socketDeviceMap = {}; 
 
-const cors = require('cors');
-
+// Enable CORS
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST']
 }));
 
-
+// Initialize Socket.IO on the same server instance
 const io = new Server(server, {
     cors: {
         origin: "*",
@@ -22,15 +22,16 @@ const io = new Server(server, {
     }
 });
 
-// app.get("/", (req, res) => res.send("server running"));
-
+// Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/home.html');
 });
 
+// Handle Socket.IO connections
 io.on('connection', (socket) => {
     console.log('A user connected with socket id:', socket.id);
 
+    // Handle device events
     socket.on('device', (data) => {
         const { deviceId, deviceStatus } = data;
         
@@ -51,6 +52,7 @@ io.on('connection', (socket) => {
         io.emit('update device list', devices);
     });
 
+    // Handle disconnect events
     socket.on('disconnect', () => {
         console.log('User disconnected:', socket.id);
         const deviceId = socketDeviceMap[socket.id];
@@ -63,8 +65,7 @@ io.on('connection', (socket) => {
     });
 });
 
-
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('listening on *:3000');
 });
 
