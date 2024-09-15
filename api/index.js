ac387a4066efd8bb09d65cb9803f7bd00c771e97
ash11109 <ayushcool1275@gmail.com>
2024-09-15 17:29:22 +0530

    150920242

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ const io = new Server(server, {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/home.html');
 });
 
 io.on('connection', (socket) => {
